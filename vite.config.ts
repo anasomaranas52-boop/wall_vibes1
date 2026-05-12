976efc3ccfeb2ed7c9f66e8e6856fc7e70e1d8da
anasomaranas52-boop <anasomaranas52@gmail.com>
2026-05-12 14:49:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@theme/tailwindcss' // تأكد من وجود السطر ده
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
